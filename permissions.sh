dbe45a12a989aaa094d24bc17cfb09e6a62e32b6
Tomás Bernardin <tomibernardin@gmail.com>
2023-10-18 19:27:31 -0300

    env adittions

--- permissions.sh
+++ permissions.sh
@@ -3,7 +3,7 @@ cd /home/tomas
 
 sudo chown -R root.docker docker
 sudo chmod -R 777 docker
-sudo chown -R root.docker backup
-sudo chmod -R 777 backup
+#sudo chown -R root.docker backup
+#sudo chmod -R 777 backup
 
 echo 'Los permisos fueron actualizados.'
